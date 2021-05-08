24776fed63370a66ec598f211999c30657677dc0
Amit Gupta <ryu.forever@gmail.com>
2021-05-08 18:22:00 +0530

    Initial Commit

---
+++ src/cylForce.js
@@ -0,0 +1,50 @@
+import React from 'react';
+
+class cylForce extends React.Component {
+    constructor(props){
+        super(props);
+        this.state={
+            dia: null,
+            pr: null
+        }
+    }
+
+    // To commonize all event handling, we're getting e.target's attribute i.e. name & value
+    // where event.target's name has to be same as state's key.
+    onHandleClick=(e)=>{
+        let nm = e.target.name;
+        let vl = e.target.value;
+        this.setState(
+            {[nm]: vl}
+        );
+    }    
+
+    render(){
+        let dia = this.state.dia;
+        let pr = this.state.pr;
+        let force;
+        if (dia != null && pr != null){
+          force = (pr*Math.PI*(dia/20)**2).toFixed(2);  
+        } else {
+            force = '';
+        }
+        
+    
+        return(
+            <div>
+                <h3>Calculating Cylinder Force.</h3>
+                <br></br>
+                <label>Enter Cylinder Dia (mm): </label>
+                <input type="number" name="dia" onChange={this.onHandleClick} /> 
+                <br></br><br></br>
+                <label>Enter Pressure (bar): </label>
+                <input type="number" name="pr" onChange={this.onHandleClick}/>
+                <br></br><br></br>
+                <label>Cylinder Force (kgf): </label>
+                <label>{force}</label>              
+            </div> 
+        );        
+    }
+}
+
+export default cylForce;
