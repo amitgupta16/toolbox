b33cea0c68dadd738870cfddd6ec3d9da2bd6dd1
Amit Gupta <ryu.forever@gmail.com>
2021-05-08 20:00:13 +0530

    index.js changed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import CylForce from './cylForce1'
+import CylForce from './cylForce'
 
 ReactDOM.render(
   <div>
