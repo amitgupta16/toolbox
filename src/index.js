70567be75d66846bb9e9fa37d89e598b848d8af9
Amit Gupta <ryu.forever@gmail.com>
2021-05-08 19:53:26 +0530

    address modified

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import CylForce from './cylForce'
+import CylForce from './cylForce1'
 
 ReactDOM.render(
   <div>
